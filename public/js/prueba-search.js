8ab089259d6819c5fbde4ac12c71151753faf4b6
lilihperez <lilihperez@gmail.com>
2016-12-13 14:14:19 -0500

    funcion de buscar

--- public/js/prueba-search.js
+++ public/js/prueba-search.js
@@ -14,3 +14,32 @@ function ocultar(evento){
   $("#descripcion-filtros").addClass("ocultar");
   $("#content-servicios").removeClass("ocultar");
 }
+
+
+$('.search1').on('click', function() {
+    var address = $('.search').val();
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ 'address': address}, geocodeResult);
+});
+ 
+var geocodeResult= function(results, status) {
+    if (status == 'OK') {
+        var mapOptions = {
+            center: results[0].geometry.location,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map($("#map").get(0), mapOptions);
+        map.fitBounds(results[0].geometry.viewport);
+
+ 
+        var iconBase1 = "img/";
+        var markerOptions = { position: results[0].geometry.location, icon: iconBase1 + 'circulo.png' }
+        var marker = new google.maps.Marker(markerOptions);
+        marker.setMap(map);
+
+           
+    } else{
+
+      alert("Geocoding no tuvo éxito debido a: " + status);
+    }
+}
