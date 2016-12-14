197e5c21bd2b97b3e9db85111d0ca9000681c376
MilagrosQB <qbmilagros@gmail.com>
2016-12-14 01:15:27 -0500

    deploy

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require("express");
 var app = express();
 
 app.use(express.static(__dirname+ "/public"));
+
+app.set('port', (process.env.PORT || 3000));
+
 app.listen(3000,function(){
 	console.log("server encendido");
 });
-
-
-
