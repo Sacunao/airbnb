4ab13b3700eced931d1ccb2f3c2b48799c3740c5
MilagrosQB <qbmilagros@gmail.com>
2016-12-14 00:13:52 -0500

    juntando partes

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-	new WOW().init();
+new WOW().init();
     
      $('.lazy').slick({
         lazyLoad: 'ondemand',
@@ -6,4 +6,25 @@
         slidesToScroll: 1
       });
 
+     $('.alojamientos').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 3,
+        slidesToScroll: 1
+      });
+
+     $('.paises').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 6,
+        slidesToScroll: 1
+      });
+
+    $('.solidario').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 4,
+        slidesToScroll: 1
+      });
+
+    $('#btn-search').click(function() {
+  		window.location.href = "/prueba-search.html";
+	});
 
