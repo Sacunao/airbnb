4975efa21461b406d77665e85f49716dfa95d4f9
MilagrosQB <qbmilagros@gmail.com>
2016-12-14 10:05:35 -0500

    changes

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static(__dirname+ "/public"));
-app.listen(3000,function(){
-	console.log("server encendido");
+app.set("port",(process.env.PORT || 8000));
+app.use(express.static(__dirname + "/public"));
+
+app.listen(app.get("port"), function(){
+  console.log("Servidor corriendo en ", app.get("port"));
 });
 
 
