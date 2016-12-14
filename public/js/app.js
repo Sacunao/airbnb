030dce07042ffb393df58fbfd96fe38fe1a1b868
sacunao <sheila.aobregon@gmail.com>
2016-12-13 21:55:48 -0500

    cambios html

--- public/js/app.js
+++ public/js/app.js
@@ -6,4 +6,22 @@
         slidesToScroll: 1
       });
 
+     $('.alojamientos').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 3,
+        slidesToScroll: 1
+      });
+
+     $('.paises').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 6,
+        slidesToScroll: 1
+      });
+
+    $('.solidario').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 4,
+        slidesToScroll: 1
+      });
+
 
