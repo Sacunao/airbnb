e9995fe63ae79a343028583e67ee9284b408d216
MilagrosQB <qbmilagros@gmail.com>
2016-12-14 00:18:18 -0500

    juntando partes de htmls

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,30 @@
-$(document).on('ready', function() {
-      $('.lazy').slick({
+new WOW().init();
+    
+     $('.lazy').slick({
         lazyLoad: 'ondemand',
         slidesToShow: 4,
         slidesToScroll: 1
       });
-    });
+
+     $('.alojamientos').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 3,
+        slidesToScroll: 1
+      });
+
+     $('.paises').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 6,
+        slidesToScroll: 1
+      });
+
+    $('.solidario').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 4,
+        slidesToScroll: 1
+      });
+
+    $('#btn-search').click(function() {
+  		window.location.href = "/prueba-search.html";
+	});
+
